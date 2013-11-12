a6068c4e6a1342fd9a8e1e5f1ca96b4cb2bd594f
Adei Josol <adjl@users.noreply.github.com>
2013-11-12 23:37:36 +0000

    Refactor

--- polynomial-arithmetic.lisp
+++ polynomial-arithmetic.lisp
@@ -1,12 +1,13 @@
+(defun make-poly (terms) (list 'poly terms))
+(defun make-term-list terms terms)
+
 (defun make-term (coeff vars) (list 'term coeff vars))
 (defun make-var-list vars vars)
+
 (defun make-var (var power) (cons var power))
 
 (defun coeff (term) (cadr term))
 (defun vars (term) (caddr term))
-
-(defun make-poly (terms) (list 'poly terms))
-(defun make-term-list terms terms)
 
 (defun term+ (term1 term2)
   (make-term (+ (coeff term1) (coeff term2))
