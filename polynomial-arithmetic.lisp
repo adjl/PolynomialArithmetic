8cdc548ede2af70d7021f48be34c4ed1bc965cb8
Adei Josol <adjl@users.noreply.github.com>
2013-11-12 19:13:03 +0000

    Implement make-term function

---
+++ polynomial-arithmetic.lisp
@@ -0,0 +1,3 @@
+(defun make-term (coeff vars) (list 'term coeff vars))
+(defun make-var-list vars vars)
+(defun make-var (var power) (cons var power))
--- test-case-polynomial-arithmetic.lisp
+++ test-case-polynomial-arithmetic.lisp
@@ -1,4 +1,39 @@
 (load "polynomial-arithmetic.lisp")
 
 (test-case 'test-case-polynomial-arithmetic
-           '())
+           '((test test-make-term
+                   ((assert-equal '(term 1 ())
+                                  (make-term 1 (make-var-list)))
+
+                    (assert-equal '(term 1 ((x . 1)))
+                                  (make-term 1 (make-var-list (make-var 'x 1))))
+
+                    (assert-equal '(term 2 ((x . 1)))
+                                  (make-term 2 (make-var-list (make-var 'x 1))))
+
+                    (assert-equal '(term 1 ((x . 2)))
+                                  (make-term 1 (make-var-list (make-var 'x 2))))
+
+                    (assert-equal '(term 2 ((x . 2)))
+                                  (make-term 2 (make-var-list (make-var 'x 2))))
+
+                    (assert-equal '(term 1 ((x . 1) (y . 1)))
+                                  (make-term 1 (make-var-list (make-var 'x 1)
+                                                              (make-var 'y 1))))
+
+                    (assert-equal '(term 2 ((x . 1) (y . 1)))
+                                  (make-term 2 (make-var-list (make-var 'x 1)
+                                                              (make-var 'y 1))))
+
+                    (assert-equal '(term 1 ((x . 2) (y . 1)))
+                                  (make-term 1 (make-var-list (make-var 'x 2)
+                                                              (make-var 'y 1))))
+
+                    (assert-equal '(term 1 ((x . 2) (y . 2)))
+                                  (make-term 1 (make-var-list (make-var 'x 2)
+                                                              (make-var 'y 2))))
+
+                    (assert-equal '(term 2 ((x . 2) (y . 2)))
+                                  (make-term 2 (make-var-list (make-var 'x 2)
+                                                              (make-var 'y 2))))
+                    ))))
