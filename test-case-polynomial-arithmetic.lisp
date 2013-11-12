51bac6b367fea053577cf59191505d3f99c3bfaf
Adei Josol <adjl@users.noreply.github.com>
2013-11-12 21:09:16 +0000

    Refactor and refine tests

--- test-case-polynomial-arithmetic.lisp
+++ test-case-polynomial-arithmetic.lisp
@@ -1,51 +1,62 @@
 (load "polynomial-arithmetic.lisp")
 
 (test-case 'test-case-polynomial-arithmetic
-           '((test test-make-term
+           '((test test-make-term-with-constant
                    ((assert-equal '(term 0 ())  (make-term 0 (make-var-list)))
                     (assert-equal '(term 1 ())  (make-term 1 (make-var-list)))
-                    (assert-equal '(term -1 ()) (make-term -1 (make-var-list)))
+                    (assert-equal '(term -2 ()) (make-term -2 (make-var-list)))))
 
-                    (assert-equal '(term 1 ((x . 1)))
+             (test test-make-term-with-single-variable
+                   ((assert-equal '(term 1 ((x . 1)))
                                   (make-term 1 (make-var-list (make-var 'x 1))))
-
                     (assert-equal '(term 2 ((x . 1)))
                                   (make-term 2 (make-var-list (make-var 'x 1))))
-
                     (assert-equal '(term 1 ((x . 2)))
                                   (make-term 1 (make-var-list (make-var 'x 2))))
-
                     (assert-equal '(term 2 ((x . 2)))
-                                  (make-term 2 (make-var-list (make-var 'x 2))))
+                                  (make-term 2 (make-var-list (make-var 'x 2))))))
 
-                    (assert-equal '(term 1 ((x . 1) (y . 1)))
+             (test test-make-term-with-multiple-variables
+                   ((assert-equal '(term 1 ((x . 1) (y . 1)))
                                   (make-term 1 (make-var-list (make-var 'x 1)
                                                               (make-var 'y 1))))
-
                     (assert-equal '(term 2 ((x . 1) (y . 1)))
                                   (make-term 2 (make-var-list (make-var 'x 1)
                                                               (make-var 'y 1))))
-
                     (assert-equal '(term 1 ((x . 2) (y . 1)))
                                   (make-term 1 (make-var-list (make-var 'x 2)
                                                               (make-var 'y 1))))
-
                     (assert-equal '(term 1 ((x . 2) (y . 2)))
                                   (make-term 1 (make-var-list (make-var 'x 2)
                                                               (make-var 'y 2))))
-
                     (assert-equal '(term 2 ((x . 2) (y . 2)))
                                   (make-term 2 (make-var-list (make-var 'x 2)
                                                               (make-var 'y 2))))))
 
-             (test test-coeff
-                   ((assert-equal '0  (coeff '(term 0 ())))
-                    (assert-equal '1  (coeff '(term 1 ((x . 1)))))
-                    (assert-equal '-2 (coeff '(term -2 ((x . 2)))))))
+             (test test-coeff-with-constant
+                   ((assert-equal '0                 (coeff '(term 0 ())))
+                    (assert-equal '1                 (coeff '(term 1 ())))
+                    (assert-equal '-2                (coeff '(term -2 ())))))
 
-             (test test-vars
+             (test test-coeff-with-single-variable
+                   ((assert-equal '1                 (coeff '(term 1 ((x . 1)))))
+                    (assert-equal '2                 (coeff '(term 2 ((x . 2)))))))
+
+             (test test-coeff-with-multiple-variables
+                   ((assert-equal '1                 (coeff '(term 1 ((x . 1)) (y . 1))))
+                    (assert-equal '2                 (coeff '(term 2 ((x . 2)) (y . 1))))
+                    (assert-equal '3                 (coeff '(term 3 ((x . 2)) (y . 2))))))
+
+             (test test-vars-with-constant
                    ((assert-equal '()                (vars '(term 0 ())))
-                    (assert-equal '((x . 1))         (vars '(term 1 ((x . 1)))))
-                    (assert-equal '((x . 2))         (vars '(term 2 ((x . 2)))))
-                    (assert-equal '((x . 2) (y . 2)) (vars '(term 3 ((x . 2) (y . 2)))))
-                    ))))
+                    (assert-equal '()                (vars '(term 1 ())))
+                    (assert-equal '()                (vars '(term -2 ())))))
+
+             (test test-vars-with-single-variable
+                   ((assert-equal '((x . 1))         (vars '(term 1 ((x . 1)))))
+                    (assert-equal '((x . 2))         (vars '(term 2 ((x . 2)))))))
+
+             (test test-vars-with-multiple-variables
+                   ((assert-equal '((x . 1) (y . 1)) (vars '(term 1 ((x . 1) (y . 1)))))
+                    (assert-equal '((x . 2) (y . 1)) (vars '(term 2 ((x . 2) (y . 1)))))
+                    (assert-equal '((x . 2) (y . 2)) (vars '(term 3 ((x . 2) (y . 2)))))))))
