66f0cb5a4a0e359162359ea763775ead7159f62c
Adei Josol <adjl@users.noreply.github.com>
2013-11-13 20:27:20 +0000

    Test qsort with and without getter

--- test-case-qsort.lisp
+++ test-case-qsort.lisp
@@ -1,7 +1,10 @@
 (test-case 'test-case-qsort
-           '((test test-qsort
+           '((test test-qsort-without-getter
                    ((assert-equal '() (qsort '()))
                     (assert-equal '(1) (qsort '(1)))
                     (assert-equal '(1 2) (qsort '(1 2)))
                     (assert-equal '(1 2 3) (qsort '(3 1 2)))
-                    (assert-equal '(1 2 2 3) (qsort '(2 3 1 2)))))))
+                    (assert-equal '(1 2 2 3) (qsort '(2 3 1 2)))))
+             (test test-qsort-with-getter
+                   ((assert-equal '((1 . 4) (2 . 3)) (qsort '((2 . 3) (1 . 4)) car))
+                    (assert-equal '((2 . 3) (1 . 4)) (qsort '((1 . 4) (2 . 3)) cdr))))))
