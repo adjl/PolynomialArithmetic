1701bbf39a0222dac424915ac8fb2f68f4cd3778
Adei Josol <adjl@users.noreply.github.com>
2013-11-13 03:34:50 +0000

    Implement automatic sorting of vars when constructing terms

--- polynomial-arithmetic.lisp
+++ polynomial-arithmetic.lisp
@@ -1,7 +1,12 @@
+(load "qsort.lisp")
+
+(defun var-sym (var) (convert (car var) <string>))
+(defun sort-vars (vars) (qsort vars var-sym))
+
 (defun make-poly (terms) (list 'poly terms))
 (defun make-term-list terms terms)
 
-(defun make-term (coeff vars) (list 'term coeff vars))
+(defun make-term (coeff vars) (list 'term coeff (sort-vars vars)))
 (defun make-var-list vars vars)
 
 (defun make-var (var power) (cons var power))
@@ -21,3 +26,6 @@
          (make-term (- (coeff term1) (coeff term2)) (vars term1)))
         (t (make-poly (make-term-list term1
                                       (make-term (- (coeff term2)) (vars term2)))))))
+
+(defun term* (term1 term2)
+  (make-term (* (coeff term1) (coeff term2)) (vars term1)))
