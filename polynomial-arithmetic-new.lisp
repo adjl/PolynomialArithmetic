7d2f24f6b8cc3271af3165cb7ebbac100467c016
Adei Josol <adjl@users.noreply.github.com>
2013-11-13 23:56:02 +0000

    Refactor new implementation

--- polynomial-arithmetic-new.lisp
+++ polynomial-arithmetic-new.lisp
@@ -1,5 +1,28 @@
 (load "qsort.lisp")
 
+(defun make-poly terms
+  (list 'poly (polyreduce (sort-by-order terms))))
+
+(defun terms (poly) (cadr poly))
+
+(defun polyreduce (terms)
+  (cond ((null terms) '())
+        ((same-orderp (car terms) (cadr terms))
+         (cons (term+ (car terms) (cadr terms)) (polyreduce (cddr terms))))
+        (t (cons (car terms) (polyreduce (cdr terms))))))
+
+(defun same-orderp (term1 term2)
+  (equal (vars term1) (vars term2)))
+
+(defun sort-by-order (terms)
+  (qsort (qsort terms term-sym) term-power))
+
+(defun term-sym (term)
+  (sym->val (sym (car (vars term)))))
+
+(defun term-power (term)
+  (pwr (car (vars term))))
+
 (defun poly+ (poly1 poly2)
   (make-poly (append (terms poly1) (terms poly2))))
 
@@ -10,20 +33,14 @@
 (defun poly* (poly1 poly2)
   (make-poly (terms* (terms poly1) (terms poly2))))
 
-(defun make-poly terms
-  (list 'poly (polyreduce (sort-by-order terms))))
+(defun make-term (coeff . vars)
+  (list 'term coeff (sort-by-sym vars)))
 
-(defun terms (poly) (cadr poly))
+(defun coeff (term) (cadr term))
+(defun vars (term) (caddr term))
 
-(defun polyreduce (terms)
-  (cond ((null terms) '())
-        ((same-orderp (car terms) (cadr terms))
-         (cons (term+ (car terms) (cadr terms))
-               (polyreduce (cddr terms))))
-        (t (cons (car terms) (polyreduce (cdr terms))))))
-
-(defun same-orderp (term1 term2)
-  (equal (vars term1) (vars term2)))
+(defun sort-by-sym (vars)
+  (qsort vars sym->val))
 
 (defun terms* (terms1 terms2)
   (reduce poly+ (terms*-inner terms1 terms2)))
@@ -42,6 +59,12 @@
 (defun term* (term1 term2)
   (make-term (* (coeff term1) (coeff term2)) (vars* (vars term1) (vars term2))))
 
+(defun make-var (sym pwr)
+  (cons sym pwr))
+
+(defun sym (var) (car var))
+(defun pwr (var) (cdr var))
+
 (defun vars* (vars1 vars2)
   (cond ((and (null vars1) (null vars2)) '())
         ((or (null vars1) (null vars2)) (or vars1 vars2))
@@ -49,32 +72,8 @@
          (cons (car vars1) (vars* (cdr vars1) vars2)))
         ((> (sym->val (sym (car vars1))) (sym->val (sym (car vars2))))
          (cons (car vars2) (vars* vars1 (cdr vars2))))
-        (t (cons (make-var (sym (car vars1)) (+ (power (car vars1)) (power (car vars2))))
+        (t (cons (make-var (sym (car vars1)) (+ (pwr (car vars1)) (pwr (car vars2))))
                  (vars* (cdr vars1) (cdr vars2))))))
-
-(defun make-term (coeff . vars)
-  (list 'term coeff (sort-by-sym vars)))
-
-(defun coeff (term) (cadr term))
-(defun vars (term) (caddr term))
-
-(defun make-var (sym power)
-  (cons sym power))
-
-(defun sym (var) (car var))
-(defun power (var) (cdr var))
 
 (defun sym->val (sym)
   (convert sym <string>))
-
-(defun sort-by-order (terms)
-  (qsort (qsort terms term-sym) term-power))
-
-(defun term-sym (term)
-  (sym->val (sym (car (vars term)))))
-
-(defun term-power (term)
-  (power (car (vars term))))
-
-(defun sort-by-sym (vars)
-  (qsort vars sym->val))
