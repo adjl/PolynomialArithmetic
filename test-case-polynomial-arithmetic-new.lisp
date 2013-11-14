76cfd196b929532d7a2e5972d77d0a3fd6ac9cfb
Adei Josol <adjl@users.noreply.github.com>
2013-11-14 03:46:40 +0000

    Refactor test case

--- test-case-polynomial-arithmetic-new.lisp
+++ test-case-polynomial-arithmetic-new.lisp
@@ -1,24 +1,24 @@
 (test-case 'test-case-polynomial-arithmetic-new
            '(
              (test test-make-var
-                   ((assert-equal '(x . 1) (make-var 'x 1))))
+                   ((assert-equal '(x . 1)               (make-var 'x 1))))
 
              (test test-sym
-                   ((assert-equal 'x       (sym '(x . 1)))))
+                   ((assert-equal 'x                     (sym '(x . 1)))))
 
              (test test-pwr
-                   ((assert-equal 1        (pwr '(x . 1)))))
+                   ((assert-equal 1                      (pwr '(x . 1)))))
 
              (test test-sym->str
-                   ((assert-equal "x"      (sym->str 'x))))
+                   ((assert-equal "x"                    (sym->str 'x))))
 
              (test test-vars*
-                   ((assert-equal '()                (vars* '() '()))
-                    (assert-equal '((x . 1))         (vars* '((x . 1)) '()))
-                    (assert-equal '((y . 1))         (vars* '() '((y . 1))))
-                    (assert-equal '((x . 1) (y . 1)) (vars* '((x . 1)) '((y . 1))))
-                    (assert-equal '((x . 1) (y . 1)) (vars* '((y . 1)) '((x . 1))))
-                    (assert-equal '((x . 2))         (vars* '((x . 1)) '((x . 1))))))
+                   ((assert-equal '()                    (vars* '() '()))
+                    (assert-equal '((x . 1))             (vars* '((x . 1)) '()))
+                    (assert-equal '((y . 1))             (vars* '() '((y . 1))))
+                    (assert-equal '((x . 1) (y . 1))     (vars* '((x . 1)) '((y . 1))))
+                    (assert-equal '((x . 1) (y . 1))     (vars* '((y . 1)) '((x . 1))))
+                    (assert-equal '((x . 2))             (vars* '((x . 1)) '((x . 1))))))
 
              (test test-sort-by-sym
                    ((assert-equal '((x . 1) (y . 1) (z . 1))
@@ -58,21 +58,17 @@
                                                                 '(1 ((y . 1)))))
                     (assert-equal '(2 ((x . 2) (y . 1))) (term* '(2 ((x . 1)))
                                                                 '(1 ((x . 1) (y . 1)))))
-
-                    (assert-equal '(2 ((x . 1) (y . 1) (z . 1)))
-                                  (term* '(2 ((x . 1))) '(1 ((y . 1) (z . 1)))))
-
                     (assert-equal '(2 ((x . 1) (y . 1))) (term* '(2 ((x . 1) (y . 1)))
                                                                 '(1 ())))
                     (assert-equal '(2 ((x . 2) (y . 1))) (term* '(2 ((x . 1) (y . 1)))
                                                                 '(1 ((x . 1)))))
-
-                    (assert-equal '(2 ((x . 1) (y . 1) (z . 1)))
-                                  (term* '(2 ((x . 1) (y . 1))) '(1 ((z . 1)))))
-
                     (assert-equal '(2 ((x . 2) (y . 2))) (term* '(2 ((x . 1) (y . 1)))
                                                                 '(1 ((x . 1) (y . 1)))))
 
+                    (assert-equal '(2 ((x . 1) (y . 1) (z . 1)))
+                                  (term* '(2 ((x . 1))) '(1 ((y . 1) (z . 1)))))
+                    (assert-equal '(2 ((x . 1) (y . 1) (z . 1)))
+                                  (term* '(2 ((x . 1) (y . 1))) '(1 ((z . 1)))))
                     (assert-equal '(2 ((x . 2) (y . 1) (z . 1)))
                                   (term* '(2 ((x . 1) (y . 1))) '(1 ((x . 1) (z . 1)))))
                     (assert-equal '(2 ((w . 1) (x . 1) (y . 1) (z . 1)))
