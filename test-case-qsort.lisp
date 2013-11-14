fdd5f6edb8a8c356736333bcfc4d5de43d89cf57
Adei Josol <adjl@users.noreply.github.com>
2013-11-14 01:54:40 +0000

    Refactor qsort test case

--- test-case-qsort.lisp
+++ test-case-qsort.lisp
@@ -1,9 +1,9 @@
 (test-case 'test-case-qsort
            '((test test-qsort-without-getter
-                   ((assert-equal '() (qsort '()))
-                    (assert-equal '(1) (qsort '(1)))
-                    (assert-equal '(1 2) (qsort '(1 2)))
-                    (assert-equal '(1 2 3) (qsort '(3 1 2)))
+                   ((assert-equal '()        (qsort '()))
+                    (assert-equal '(1)       (qsort '(1)))
+                    (assert-equal '(1 2)     (qsort '(1 2)))
+                    (assert-equal '(1 2 3)   (qsort '(3 1 2)))
                     (assert-equal '(1 2 2 3) (qsort '(2 3 1 2)))))
              (test test-qsort-with-getter
                    ((assert-equal '((1 . 4) (2 . 3))
