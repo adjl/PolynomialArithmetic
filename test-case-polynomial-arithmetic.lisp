b66033c6380cdf61c087829c37201e9bc1c121dc
Adei Josol <adjl@users.noreply.github.com>
2013-12-24 18:59:53 +0000

    Add var-simplify test

--- test-case-polynomial-arithmetic.lisp
+++ test-case-polynomial-arithmetic.lisp
@@ -4,4 +4,8 @@
       test-make-var
       ((assert-equal '()       (make-var 'x 0))
        (assert-equal '(x . 1)  (make-var 'x 1))))
+    (test
+      test-var-simplify
+      ((assert-equal '()       (var-simplify '(x . 0)))
+       (assert-equal '(x . 1)  (var-simplify '(x . 1)))))
     ))
