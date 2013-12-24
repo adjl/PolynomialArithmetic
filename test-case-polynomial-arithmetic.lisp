089c5055a1dbb320d1f696168107591dc297e077
Adei Josol <adjl@users.noreply.github.com>
2013-12-24 18:51:46 +0000

    Add make-var test

--- test-case-polynomial-arithmetic.lisp
+++ test-case-polynomial-arithmetic.lisp
@@ -1,3 +1,7 @@
 (test-case
   'test-case-polynomial-arithmetic
-  '())
+  '((test
+      test-make-var
+      ((assert-equal '()       (make-var 'x 0))
+       (assert-equal '(x . 1)  (make-var 'x 1))))
+    ))
