d88ac76bead92cdacf04e09d512ea87e6e2a03ad
Adei Josol <adjl@users.noreply.github.com>
2013-12-25 03:44:05 +0000

    Add coeff test

--- test-case-polynomial-arithmetic.lisp
+++ test-case-polynomial-arithmetic.lisp
@@ -16,6 +16,13 @@
       test-pwr
       ((assert-equal 1  (pwr (make-var 'x 1)))))
     (test
+      test-make-varlist
+      ((assert-equal '()         (make-varlist '()))
+       (assert-equal '(())       (make-varlist '((make-var 'x 0))))
+       (assert-equal '((x . 1))  (make-varlist '((make-var 'x 1))))
+       (assert-equal '((x . 1) (y . 1))
+                     (make-varlist '((make-var 'x 1) (make-var 'y 1))))))
+    (test
       test-make-term
       ((assert-equal '()             (make-term 0 '()))
        (assert-equal '(1 ())         (make-term 1 '()))
@@ -24,14 +31,10 @@
        (assert-equal '(4 ((x . 1) (y . 1)))
                      (make-term 4 '((make-var 'x 1) (make-var 'y 1))))))
     (test
-      test-make-varlist
-      ((assert-equal '()         (make-varlist '()))
-       (assert-equal '(())       (make-varlist '((make-var 'x 0))))
-       (assert-equal '((x . 1))  (make-varlist '((make-var 'x 1))))
-       (assert-equal '((x . 1) (y . 1))
-                     (make-varlist '((make-var 'x 1) (make-var 'y 1))))))
-    (test
       test-term-simplify
       ((assert-equal '()             (term-simplify '(0 ((x . 1)))))
        (assert-equal '(1 ((x . 1)))  (term-simplify '(1 ((x . 1)))))))
+    (test
+      test-coeff
+      ((assert-equal 1  (coeff (make-term 1 '((make-var 'x 1)))))))
     ))
