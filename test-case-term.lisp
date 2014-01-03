4305d7506aa1b7bee91a566aea73c4e2e44f6ef4
Adei Josol <adjl@users.noreply.github.com>
2014-01-03 00:17:20 +0000

    Reformat term test case

--- test-case-term.lisp
+++ test-case-term.lisp
@@ -1,6 +1,11 @@
 (test-case
   'test-case-term
   '((test
+      test-termsimplify
+      ((assert-equal '()             (termsimplify '(0 ((x . 1)))))
+       (assert-equal '(1 ((x . 1)))  (termsimplify '(1 ((x . 1)))))))
+    ; make-term-internal
+    (test
       test-make-term
       ((assert-equal '()             (make-term 0 '()))
        (assert-equal '(1 ())         (make-term 1 '()))
@@ -9,27 +14,11 @@
        (assert-equal '(4 ((x . 1) (y . 1)))
                      (make-term 4 '((make-var 'x 1) (make-var 'y 1))))))
     (test
-      test-termsimplify
-      ((assert-equal '()             (termsimplify '(0 ((x . 1)))))
-       (assert-equal '(1 ((x . 1)))  (termsimplify '(1 ((x . 1)))))))
-    (test
       test-coeff
       ((assert-equal 1  (coeff (make-term 1 '((make-var 'x 1)))))))
     (test
       test-vars
       ((assert-equal '((x . 1))  (vars (make-term 1 '((make-var 'x 1)))))))
-    (test
-      test-equal-orderp
-      ((assert-equal t    ((equal-orderp id (make-term 1 '()))
-                           (make-term 2 '())))
-       (assert-equal t    ((equal-orderp id (make-term 1 '((make-var 'x 1))))
-                           (make-term 2 '((make-var 'x 1)))))
-       (assert-equal t    ((equal-orderp id (make-term 1 '((make-var 'x 2))))
-                           (make-term 2 '((make-var 'x 2)))))
-       (assert-equal t    ((equal-orderp id (make-term 1 '((make-var 'x 1)
-                                                          (make-var 'y 1))))
-                           (make-term 2 '((make-var 'x 1)
-                                          (make-var 'y 1)))))))
     (test
       test-term+
       ((assert-equal '(2 ())         (term+ (make-term 1 '()) (make-term 1 '())))
@@ -40,14 +29,6 @@
        (assert-equal '(2 ((x . 1) (y . 1)))
                      (term+ (make-term 1 '((make-var 'x 1) (make-var 'y 1)))
                             (make-term 1 '((make-var 'x 1) (make-var 'y 1)))))))
-    (test
-      test-termreduce
-      ((assert-equal '()               (termreduce '()))
-       (assert-equal '((1 ((x . 1))))  (termreduce '((1 ((x . 1))))))
-       (assert-equal '((2 ((x . 1))))  (termreduce '((1 ((x . 1)))
-                                                     (1 ((x . 1))))))
-       (assert-equal '((1 ((x . 1))) (1 ((y . 1))))
-                     (termreduce '((1 ((x . 1))) (1 ((y . 1))))))))
     (test
       test-term*
       ((assert-equal '(1 ((x . 1)))  (term* (make-term 1 '())
@@ -60,20 +41,30 @@
                      (term* (make-term 2 '((make-var 'x 1)))
                             (make-term 2 '((make-var 'y 1)))))))
     (test
-      test-term*-out
-      ((assert-equal '(1 ((x . 1)))  ((term*-out (make-term 1 '()))
-                                      (make-term 1 '((make-var 'x 1)))))
-       (assert-equal '(2 ((x . 1)))  ((term*-out (make-term 1 '((make-var 'x 1))))
-                                      (make-term 2 '())))
-       (assert-equal '(3 ((x . 2)))  ((term*-out (make-term 3 '((make-var 'x 1))))
-                                      (make-term 1 '((make-var 'x 1)))))
-       (assert-equal '(4 ((x . 1) (y . 1)))
-                     ((term*-out (make-term 2 '((make-var 'y 1))))
-                                 (make-term 2 '((make-var 'x 1)))))))
-    (test
       test-termnegate
       ((assert-equal '(-1 ((x . 1)))  (termnegate (make-term 1 '((make-var 'x 1)))))
        (assert-equal '(1  ((x . 1)))  (termnegate (make-term -1 '((make-var 'x 1)))))))
+    (test
+      test-equal-orderp
+      ((assert-equal t    ((equal-orderp id (make-term 1 '()))
+                           (make-term 2 '())))
+       (assert-equal t    ((equal-orderp id (make-term 1 '((make-var 'x 1))))
+                           (make-term 2 '((make-var 'x 1)))))
+       (assert-equal t    ((equal-orderp id (make-term 1 '((make-var 'x 2))))
+                           (make-term 2 '((make-var 'x 2)))))
+       (assert-equal t    ((equal-orderp id (make-term 1 '((make-var 'x 1)
+                                                          (make-var 'y 1))))
+                           (make-term 2 '((make-var 'x 1)
+                                          (make-var 'y 1)))))))
+    ; termsort
+    (test
+      test-termreduce
+      ((assert-equal '()               (termreduce '()))
+       (assert-equal '((1 ((x . 1))))  (termreduce '((1 ((x . 1))))))
+       (assert-equal '((2 ((x . 1))))  (termreduce '((1 ((x . 1)))
+                                                     (1 ((x . 1))))))
+       (assert-equal '((1 ((x . 1))) (1 ((y . 1))))
+                     (termreduce '((1 ((x . 1))) (1 ((y . 1))))))))
     (test
       test-termlist*
       ((assert-equal '(((1 ((x . 1))) (1 ())))
