99fa2f7cf88ab7e0e17605781b6e7688fd1b4432
Adei Josol <adjl@users.noreply.github.com>
2014-01-03 12:07:11 +0000

    Write more poly tests

--- test-case-poly.lisp
+++ test-case-poly.lisp
@@ -1,26 +1,15 @@
 (test-case
   'test-case-poly
-  '((test
-      test-make-poly
-      ((assert-equal nil
+  '(; These tests are for functions and operations on polynomials
+    (test
+      test-make-poly ; Call termreduce internally
+      (; 0 == '()
+       (assert-equal nil
                      (make-poly nil))
+       ; 0 == '()
        (assert-equal nil
                      (make-poly '((make-term 0 nil))))
-       (assert-equal '((1 ()))
-                     (make-poly '((make-term 1 nil))))
-       (assert-equal '((2 ((x . 1)))
-                       (1 ()))
-                     (make-poly
-                       '((make-term 2 '((make-var 'x 1)))
-                         (make-term 1 nil))))
-       (assert-equal '((3 ((x . 2)))
-                       (4 ((x . 1) (y . 1)))
-                       (1 ()))
-                     (make-poly
-                       '((make-term 3 '((make-var 'x 2)))
-                         (make-term 4 '((make-var 'x 1)
-                                        (make-var 'y 1)))
-                         (make-term 1 nil))))
+       ; 3x^2 + 4xy + 2x + 1
        (assert-equal '((3 ((x . 2)))
                        (4 ((x . 1) (y . 1)))
                        (2 ((x . 1)))
@@ -30,15 +19,77 @@
                          (make-term 4 '((make-var 'x 1)
                                         (make-var 'y 1)))
                          (make-term 2 '((make-var 'x 1)))
-                         (make-term 1 nil))))))
+                         (make-term 1 nil))))
+       ; 6x^2 + 8xy + 4x + 2
+       ; == 3x^2 + 4xy + 2x + 1 + 3x^2 + 4xy + 2x + 1
+       (assert-equal '((6 ((x . 2)))
+                       (8 ((x . 1) (y . 1)))
+                       (4 ((x . 1)))
+                       (2 ()))
+                     (make-poly
+                       '((make-term 3 '((make-var 'x 2)))
+                         (make-term 4 '((make-var 'x 1)
+                                        (make-var 'y 1)))
+                         (make-term 2 '((make-var 'x 1)))
+                         (make-term 1 nil)
+                         (make-term 3 '((make-var 'x 2)))
+                         (make-term 4 '((make-var 'x 1)
+                                        (make-var 'y 1)))
+                         (make-term 2 '((make-var 'x 1)))
+                         (make-term 1 nil))))
+       ; 5x^2 + 4xy + 2x + 3
+       ; == 3x^2 + 4xy + 2x + 1 + 2x^2 + 2
+       (assert-equal '((5 ((x . 2)))
+                       (4 ((x . 1) (y . 1)))
+                       (2 ((x . 1)))
+                       (3 ()))
+                     (make-poly
+                       '((make-term 3 '((make-var 'x 2)))
+                         (make-term 4 '((make-var 'x 1)
+                                        (make-var 'y 1)))
+                         (make-term 2 '((make-var 'x 1)))
+                         (make-term 1 nil)
+                         (make-term 2 '((make-var 'x 2)))
+                         (make-term 2 nil))))
+       ; 0 == 3x^2 + 4xy + 2x + 1 - 3x^2 - 4xy - 2x - 1
+       (assert-equal nil
+                     (make-poly
+                       '((make-term 3 '((make-var 'x 2)))
+                         (make-term 4 '((make-var 'x 1)
+                                        (make-var 'y 1)))
+                         (make-term 2 '((make-var 'x 1)))
+                         (make-term 1 nil)
+                         (make-term -3 '((make-var 'x 2)))
+                         (make-term -4 '((make-var 'x 1)
+                                         (make-var 'y 1)))
+                         (make-term -2 '((make-var 'x 1)))
+                         (make-term -1 nil))))
+       ; x^2 + 2xy - 1
+       ; == 3x^2 + 4xy + 2x + 1 - 2x^2 - 2xy - 2x - 2
+       (assert-equal '((1 ((x . 2)))
+                       (2 ((x . 1) (y . 1)))
+                       (-1 ()))
+                     (make-poly
+                       '((make-term 3 '((make-var 'x 2)))
+                         (make-term 4 '((make-var 'x 1)
+                                        (make-var 'y 1)))
+                         (make-term 2 '((make-var 'x 1)))
+                         (make-term 1 nil)
+                         (make-term -2 '((make-var 'x 2)))
+                         (make-term -2 '((make-var 'x 1)
+                                         (make-var 'y 1)))
+                         (make-term -2 '((make-var 'x 1)))
+                         (make-term -2 nil))))))
     (test
       test-polynegate
-      ((assert-equal nil
+      (; -0 == 0 == '()
+       (assert-equal nil
                      (polynegate (make-poly nil)))
+       ; -3x^2 - 4xy - 2x - 1 == (3x^2 + 4xy + 2x + 1) * -1
        (assert-equal (make-poly
                        '((make-term -3 '((make-var 'x 2)))
                          (make-term -4 '((make-var 'x 1)
-                                        (make-var 'y 1)))
+                                         (make-var 'y 1)))
                          (make-term -2 '((make-var 'x 1)))
                          (make-term -1 nil)))
                      (polynegate
@@ -49,9 +100,13 @@
                            (make-term 2 '((make-var 'x 1)))
                            (make-term 1 nil)))))))
     (test
-      test-poly+
-      ((assert-equal nil
+      test-poly+ ; Call termreduce internally
+                 ; Exceptional use cases are already covered in
+                 ; test-make-poly and test-termreduce
+      (; 0 == 0 + 0
+       (assert-equal nil
                      (poly+ nil nil))
+       ; x + y + 1 == 0 + (x + y + 1)
        (assert-equal (make-poly
                        '((make-term 1 '((make-var 'x 1)))
                          (make-term 1 '((make-var 'y 1)))
@@ -62,6 +117,7 @@
                          '((make-term 1 '((make-var 'x 1)))
                            (make-term 1 '((make-var 'y 1)))
                            (make-term 1 nil)))))
+       ; x + y + 1 == (x + y + 1) + 0
        (assert-equal (make-poly
                        '((make-term 1 '((make-var 'x 1)))
                          (make-term 1 '((make-var 'y 1)))
@@ -72,6 +128,7 @@
                            (make-term 1 '((make-var 'y 1)))
                            (make-term 1 nil)))
                        nil))
+       ; 2x + 2y + 2 == (x + y + 1) + (x + y + 1)
        (assert-equal (make-poly
                        '((make-term 2 '((make-var 'x 1)))
                          (make-term 2 '((make-var 'y 1)))
@@ -86,9 +143,14 @@
                            (make-term 1 '((make-var 'y 1)))
                            (make-term 1 nil)))))))
     (test
-      test-poly-
-      ((assert-equal nil
+      test-poly- ; Call poly+ and polynegate
+                 ; Exceptional use cases are already covered in
+                 ; test-make-poly, test-polynegate and
+                 ; test-termreduce
+      (; 0 == 0 - 0
+       (assert-equal nil
                      (poly- nil nil))
+       ; -x - y - 1 == 0 - (x + y + 1)
        (assert-equal (make-poly
                        '((make-term -1 '((make-var 'x 1)))
                          (make-term -1 '((make-var 'y 1)))
@@ -99,6 +161,7 @@
                          '((make-term 1 '((make-var 'x 1)))
                            (make-term 1 '((make-var 'y 1)))
                            (make-term 1 nil)))))
+       ; x + y + 1 == (x + y + 1) - 0
        (assert-equal (make-poly
                        '((make-term 1 '((make-var 'x 1)))
                          (make-term 1 '((make-var 'y 1)))
@@ -109,6 +172,7 @@
                            (make-term 1 '((make-var 'y 1)))
                            (make-term 1 nil)))
                        nil))
+       ; 0 == (x + y + 1) - (x + y + 1)
        (assert-equal nil
                      (poly-
                        (make-poly
@@ -118,6 +182,12 @@
                        (make-poly
                          '((make-term 1 '((make-var 'x 1)))
                            (make-term 1 '((make-var 'y 1)))
-                           (make-term 1 nil)))))
+                           (make-term 1 nil)))))))
+    (test
+      test-poly* ; Call poly+ and termlist* internally
+                 ; Exceptional use cases are already covered in
+                 ; test-make-poly, test-termreduce and
+                 ; test-termlist*
+      (
        ))
     ))
